e9df7052442e855445694967b9c452316af7d872
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-06-03 11:13:36 +0700

    1st push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    getAPIFromGit()
+  }, [])
+
+
+  async function getAPIFromGit() {
+    let url = `https://api.github.com/authorizations`
+    let res = await Axios.get(url)
+    console.log(res.data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
